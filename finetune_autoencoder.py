32d334c57394205dd3f052e003e2b66eff75b6fb
Masao Taketani <masaotaket003@gmail.com>
2025-09-28 14:47:34 +0900

    make it compatible with multi-gpu training with Accelerate

--- finetune_autoencoder.py
+++ finetune_autoencoder.py
@@ -4,7 +4,6 @@ import argparse
 import math
 import time
 import random
-
 import torch
 import torch.nn.functional as F
 from diffusers import AutoencoderKL
@@ -12,12 +11,13 @@ from torch import optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import get_cosine_schedule_with_warmup
-from config_sd import PRETRAINED_MODEL_NAME_OR_PATH
-from dataset import preprocess_train
 from datasets import Dataset
 import datasets
-
 import wandb
+from accelerate import Accelerator
+
+from config_sd import PRETRAINED_MODEL_NAME_OR_PATH
+from dataset import preprocess_train
 
 
 def parse_args():
@@ -104,7 +104,7 @@ def parse_args():
         type=str,
         default="constant",
         help=(
-            'The scheduler type to use. Choose between ["cosine", "constant", "constant_with_warmup"]'
+            'The scheduler type to use. Choose between ["cosine", "constant", "cosine_with_warmup"]'
         ),
     )
     parser.add_argument(
@@ -213,7 +213,8 @@ def update_chunk_id(chunk_id, num_chunks):
 
 def main():
     args = parse_args()
-    if args.report_to == "wandb":
+    accelerator = Accelerator()
+    if accelerator.is_main_process and args.report_to == "wandb":
         import wandb
         wandb.init(
             project="gamengen-vae-training",
@@ -231,6 +232,8 @@ def main():
             name=f"vae-finetuning-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}",
         )
 
+    device = accelerator.device
+
     parquet_paths = collect_all_parquet_files(args.dataset_basepath)
     random.shuffle(parquet_paths)
     samples_per_chunk = len(parquet_paths) // args.num_chunks
@@ -241,9 +244,7 @@ def main():
     dataloader = change_chunk_id_for_dataloader(plus_one_idx, parquet_paths, samples_per_chunk, chunk_id, args.batch_size, args.num_workers)
 
     # Model Setup
-    vae = AutoencoderKL.from_pretrained(PRETRAINED_MODEL_NAME_OR_PATH, subfolder="vae")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = vae.to(device)
+    model = AutoencoderKL.from_pretrained(PRETRAINED_MODEL_NAME_OR_PATH, subfolder="vae")
     make_decoder_trainable(model)
     # Optimizer Setup
     optimizer_cls = optim.AdamW if args.use_adamw else optim.Adafactor
@@ -251,12 +252,24 @@ def main():
         model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
     )
 
-    if args.lr_scheduler == "constant_with_warmup":
+    if args.lr_scheduler == "cosine_with_warmup":
         scheduler = get_cosine_schedule_with_warmup(
             optimizer,
             num_warmup_steps=NUM_WARMUP_STEPS,
             num_training_steps=NUM_EPOCHS * len(train_loader),
         )
+        dataloader, model, optimizer, scheduler = accelerator.prepare(
+            dataloader, model, optimizer, scheduler
+        )
+    else:
+        dataloader, model, optimizer = accelerator.prepare(
+            dataloader, model, optimizer
+        )
+
+    # We need to initialize the trackers we use, and also store our configuration.
+    # The trackers initializes automatically on the main process.
+    if accelerator.is_main_process:
+        accelerator.init_trackers("vae-decoder-fine-tune", config=vars(args))
 
     step = 0
     progress_bar = tqdm(
@@ -265,56 +278,56 @@ def main():
                         total=args.max_train_steps,
                         desc="Steps",
                         # Only show the progress bar once on each machine.
-                        #disable=not accelerator.is_local_main_process,
+                        disable=not accelerator.is_local_main_process,
                    )
     
     break_while = False
     while True:
         for batch in dataloader:
-            model.train()
-            data = batch["pixel_values"].to(device)
-            optimizer.zero_grad()
+            with accelerator.autocast():
+                model.train()
+                data = batch["pixel_values"].to(device)
+                optimizer.zero_grad()
 
-            """
-            The following part is not sampled as for the encoder output according to the link below.
-            https://github.com/huggingface/diffusers/blob/c586aadef6bb66d355fa40a2b95a0bea8a6fe79c/src/diffusers/models/autoencoders/autoencoder_kl.py#L438
-            Don't know why. May change it later.
-            """
-            reconstruction = model(data, sample_posterior=True).sample
-            loss = F.mse_loss(reconstruction, data, reduction="mean")
+                """
+                The following part is not sampled as for the encoder output according to the link below.
+                https://github.com/huggingface/diffusers/blob/c586aadef6bb66d355fa40a2b95a0bea8a6fe79c/src/diffusers/models/autoencoders/autoencoder_kl.py#L438
+                Don't know why. May change it later.
+                """
+                reconstruction = model(data, sample_posterior=True).sample
+                loss = F.mse_loss(reconstruction, data, reduction="mean")
 
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), args.gradient_clipping)
-            optimizer.step()
-            if args.lr_scheduler == "constant_with_warmup": scheduler.step()
+                accelerator.backward(loss)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.gradient_clipping)
+                optimizer.step()
+                log_dic = {"loss": loss.item()}
+                if args.lr_scheduler == "cosine_with_warmup": 
+                    scheduler.step()
+                    current_lr = scheduler.get_last_lr()[0]
+                    log_dic["lr"] = current_lr
+                progress_bar.set_postfix(log_dic)
+                if args.report_to == "wandb": wandb.log(log_dic)
 
-            if args.lr_scheduler == "constant_with_warmup": current_lr = scheduler.get_last_lr()[0]
+                progress_bar.update(1)
+                step += 1
 
-            log_dic = {"loss": loss.item()}
-            if args.lr_scheduler == "constant_with_warmup": log_dic["lr"] = current_lr
-            progress_bar.set_postfix(log_dic)
-            if args.report_to == "wandb": wandb.log(log_dic)
+                if accelerator.is_main_process and step % args.validation_steps == 0:
+                    # Use only two samples for validation
+                    val_loss = eval_model_mod(accelerator.unwrap_model(model), data[:2], args.report_to)
+                    # save model to hub
+                    accelerator.unwrap_model(model).save_pretrained(
+                        os.path.join(args.output_dir, "vae"),
+                        repo_id=args.repo_id if args.push_to_hub else None,
+                        push_to_hub=args.push_to_hub,
+                    )
+                    #if args.report_to == "wandb": wandb.log({"val_loss": val_loss})
 
-            progress_bar.update(1)
-            step += 1
-
-            if step % args.validation_steps == 0:
-                # Use only two samples for validation
-                val_loss = eval_model_mod(model, data[:2], args.report_to)
-                # save model to hub
-                model.save_pretrained(
-                    os.path.join(args.output_dir, "vae"),
-                    repo_id=args.repo_id if args.push_to_hub else None,
-                    push_to_hub=args.push_to_hub,
-                )
-                #if args.report_to == "wandb": wandb.log({"val_loss": val_loss})
-
-            if step >= args.max_train_steps:
-                break_while = True
-                break
-            if step % args.chunk_id_interval == 0: 
-                chunk_id = update_chunk_id(chunk_id, args.num_chunks)
-                dataloader = change_chunk_id_for_dataloader(plus_one_idx, parquet_paths, samples_per_chunk, chunk_id, args.batch_size, args.num_workers)
+                if step >= args.max_train_steps:
+                    break_while = True
+                    break
+                if step % args.chunk_id_interval == 0: 
+                    chunk_id = update_chunk_id(chunk_id, args.num_chunks)
+                    dataloader = change_chunk_id_for_dataloader(plus_one_idx, parquet_paths, samples_per_chunk, chunk_id, args.batch_size, args.num_workers)
         
         if break_while: break
 
